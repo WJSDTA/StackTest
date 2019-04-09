7c18329c386ecadf8d147c714b6360cfeb5c482e
WJSDTA <WJSDTA@gmail.com>
2019-04-09 20:11:08 +0800

    编写公共的接口

--- src/APITest/MainClass.java
+++ src/APITest/MainClass.java
@@ -1,5 +1,7 @@
 package APITest;
 
+import java.util.concurrent.BlockingQueue;
+
 /**
  * \* Created with IntelliJ IDEA.
  * \* User: WJ
@@ -10,6 +12,73 @@ package APITest;
  * \
  */
 public class MainClass implements Runnable{
+    private String name;
+    private Message message;
+    private BlockingQueue<Message> queue;
+    private String low;
+    private String high;
+    public Message s;
+    public String from;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Message getMessage() {
+        return message;
+    }
+
+    public void setMessage(Message message) {
+        this.message = message;
+    }
+
+    public BlockingQueue<Message> getQueue() {
+        return queue;
+    }
+
+    public void setQueue(BlockingQueue<Message> queue) {
+        this.queue = queue;
+    }
+
+    public String getLow() {
+        return low;
+    }
+
+    public void setLow(String low) {
+        this.low = low;
+    }
+
+    public String getHigh() {
+        return high;
+    }
+
+    public void setHigh(String high) {
+        this.high = high;
+    }
+
+    public Message getS() {
+        return s;
+    }
+
+    public void setS(Message s) {
+        this.s = s;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public MainClass(BlockingQueue<Message> queue) {
+        this.queue = queue;
+    }
 
     @Override
     public void run() {
